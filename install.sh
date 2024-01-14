e9834aa62f04d353e1703ddbcad492ad5661b1bf
Song Song Li <ssli@redhat.com>
2024-01-14 10:22:22 +0800

    add e2e test
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- install.sh
+++ install.sh
@@ -2,13 +2,8 @@
 
 # deploy kind cluster
 # https://kind.sigs.k8s.io/docs/user/configuration/
-# for testing
-kind delete cluster --name private-cloud
-# for testing
-kind create cluster --name private-cloud --config kind/kind-config.yaml
+kind create cluster --name private-cloud --config kind-conf/kind-config.yaml
 kind export kubeconfig --name private-cloud
-kubectl label nodes private-cloud-worker node-role.kubernetes.io/worker=worker
-kubectl label nodes private-cloud-worker2 node-role.kubernetes.io/worker=worker
 
 # deploy flux
 # https://fluxcd.io/flux/get-started/
@@ -20,10 +15,10 @@ flux bootstrap github \
   --personal \
   --private false
 
-PASSWORD="admin"
-gitops create dashboard ww-gitops \
-  --password=$PASSWORD \
-  --export > ./clusters/private-cloud/weave-gitops-dashboard.yaml
-git add -A && git commit -m "Add Weave GitOps Dashboard"
-git push
-kubectl port-forward svc/ww-gitops-weave-gitops -n flux-system 9001:9001
+# PASSWORD="admin"
+# gitops create dashboard ww-gitops \
+#   --password=$PASSWORD \
+#   --export > ./clusters/private-cloud/weave-gitops-dashboard.yaml
+# git add -A && git commit -m "Add Weave GitOps Dashboard"
+# git push
+# kubectl port-forward svc/ww-gitops-weave-gitops -n flux-system 9001:9001
--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
-echo "running e2e test ..."
-
 echo "install kind ..."
 curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.20.0/kind-$(uname)-amd64"
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
-
-echo "create kind cluster"
-kind create cluster
 
 echo "install kubectl ..."
 curl -LO "https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-echo "running e2e test ..."
-kubectl cluster-info
+echo "create kind cluster"
+kind create cluster --name private-cloud --config kind-conf/kind-config.yaml
+
+echo "install flux cli"
+curl -s https://fluxcd.io/install.sh | sudo bash
+
+echo "install flux"
+flux bootstrap github \
+  --owner=$GITHUB_USER \
+  --repository=private-cloud \
+  --branch=main \
+  --path=./clusters/private-cloud \
+  --personal \
+  --private false
+
+# echo "running e2e test ..."
+sleep 1200
+flux get all
+kubectl get po -A
