13eafa71503475251b14c5c3c2d64b27b0dfed22
Song Song Li <ssli@redhat.com>
2024-01-14 12:13:43 +0800

    update install.sh
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
 
-# deploy kind cluster
-# https://kind.sigs.k8s.io/docs/user/configuration/
-kind create cluster --name private-cloud --config kind-conf/kind-config.yaml
-kind export kubeconfig --name private-cloud
+if which kind > /dev/null 2>&1; then
+    echo "install kind ..."
+    curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.20.0/kind-$(uname)-amd64"
+    chmod +x ./kind
+    sudo mv ./kind /usr/local/bin/kind
+fi
 
-# deploy flux
+if which kubectl > /dev/null 2>&1; then
+    echo "install kubectl ..."
+    curl -LO "https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl"
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+fi
+
+if which flux > /dev/null 2>&1; then
+    # https://fluxcd.io/flux/installation/#install-the-flux-cli
+    echo "install flux cli"
+    curl -s https://fluxcd.io/install.sh | sudo bash
+fi
+
+# https://kind.sigs.k8s.io/docs/user/configuration/
+echo "create kind cluster"
+kind create cluster --name private-cloud --config kind-conf/kind-config.yaml
+
 # https://fluxcd.io/flux/get-started/
+echo "install flux"
 flux bootstrap github \
   --owner=$GITHUB_USER \
   --repository=private-cloud \
--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -1,31 +1,9 @@
 #!/usr/bin/env bash
 
-echo "install kind ..."
-curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.20.0/kind-$(uname)-amd64"
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
+./install.sh
 
-echo "install kubectl ..."
-curl -LO "https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+echo "run e2e test"
 
-echo "create kind cluster"
-kind create cluster --name private-cloud --config kind-conf/kind-config.yaml
-
-echo "install flux cli"
-curl -s https://fluxcd.io/install.sh | sudo bash
-
-echo "install flux"
-flux bootstrap github \
-  --owner=$GITHUB_USER \
-  --repository=private-cloud \
-  --branch=main \
-  --path=./clusters/private-cloud \
-  --personal \
-  --private false
-
-# echo "running e2e test ..."
 sleep 1200
 flux get all
 kubectl get po -A
