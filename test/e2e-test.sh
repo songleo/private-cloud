72ed61c4b5cfd9ea8548dedd185a0bbe2318bfbc
Song Song Li <ssli@redhat.com>
2024-01-14 16:01:50 +0800

    add test msg for ingress
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -69,19 +69,28 @@ kubectl get statefulset/awx-postgres-13 -n awx | grep '1/1' || exit 1
 kubectl get statefulset/alertmanager-main -n monitoring | grep '1/1' || exit 1
 kubectl get statefulset/prometheus-k8s -n monitoring | grep '1/1' || exit 1
 
+# for debug
+kubectl get po -A
+kubectl get ingress -A
+flux get all
+
 echo "add the mapping of IP addresses and domain name"
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 HOSTNAME="www.private-cloud.com"
 sudo sed -i "/$IP_ADDRESS /c\\$IP_ADDRESS $HOSTNAME" /etc/hosts
 
 echo "verify all ingress ..."
+echo "test nginx ingress"
 curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/nginx | grep -q 200 || exit 1
+echo "test awx ingress"
 curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/awx/#/home | grep -q 200 || exit 1
-curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/weave-gitops | grep -q 301 || exit 1
+echo "test argocd ingress"
 curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/argocd | grep -q 308 || exit 1
+echo "test prometheus ingress"
 curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/prometheus | grep -q 302 || exit 1
+echo "test grafana ingress"
 curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/grafana | grep -q 302 || exit 1
+echo "test alertmanager ingress"
 curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/alertmanager | grep -q 200 || exit 1
-
-kubectl get po -A
-flux get all
+echo "test weave-gitops ingress"
+curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/weave-gitops | grep -q 301 || exit 1
