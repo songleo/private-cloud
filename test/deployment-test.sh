6c25fa1fd6b4f1e30eaaf07ed4a386bb97b8720f
Song Song Li <ssli@redhat.com>
2024-01-29 19:15:31 +0800

    split test
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- test/deployment-test.sh
+++ test/deployment-test.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-echo "run e2e test ..."
+echo "run deployment test ..."
 
 echo "reconcile flux resource ..."
-
 flux reconcile source git flux-system
 flux reconcile source git apps
 
@@ -73,28 +72,3 @@ kubectl wait --timeout 300s --for=condition=available -n flux-system \
     deployment/weave-gitops \
     || exit 1
 
-# for debug
-kubectl get po -A
-kubectl get ingress -A
-flux get all
-
-echo "add the mapping of IP addresses and domain name"
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
-HOSTNAME="www.private-cloud.com"
-sudo sed -i "/$IP_ADDRESS /c\\$IP_ADDRESS $HOSTNAME" /etc/hosts
-
-echo "verify all ingress ..."
-echo "test nginx ingress"
-curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/nginx | grep -q 200 || exit 1
-echo "test awx ingress"
-curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/awx/#/home | grep -q 200 || exit 1
-echo "test argocd ingress"
-curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/argocd | grep -q 308 || exit 1
-echo "test prometheus ingress"
-curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/prometheus | grep -q 302 || exit 1
-echo "test grafana ingress"
-curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/grafana | grep -q 302 || exit 1
-echo "test alertmanager ingress"
-curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/alertmanager | grep -q 200 || exit 1
-echo "test weave-gitops ingress"
-curl -o /dev/null -s -w "%{http_code}" http://www.private-cloud.com/weave-gitops | grep -q 301 || exit 1
